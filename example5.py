00256caa3e44a31df1ebc3870eab56dda158d8a7
tre2man <kimtree97@naver.com>
2020-01-21 00:32:23 +0900

    example5

---
+++ example5.py
@@ -0,0 +1,24 @@
+import cv2
+
+cap = cv2.VideoCapture(-1)
+face_pattern = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face = face_pattern.detectMultiScale(gray, 1.5)
+    for (x, y, w, h) in face:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        eye_gray = gray[y:y + h, x:x + h]
+        eye_color = frame[y:y + h, x:x + h]
+        eyes = eye_cascade.detectMultiScale(eye_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(eye_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 3)
+
+    cv2.imshow('image', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
